dc71fc6de649ebc6bc807bd3770fed73acc8c17c
Vinícius Gomes <viniciusrlg13@gmail.com>
2026-06-11 22:16:58 -0300

    Fix: Forçando assetPrefix no next.config para corrigir CSS

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/atividade_cep_pweb2', 
+
+  basePath: '/atividade_cep_pweb2',
+  assetPrefix: '/atividade_cep_pweb2',
 };
 
 export default nextConfig;

6ef49b37cc57890af22fcbebb4400e11c85daad1
Vinícius Gomes <viniciusrlg13@gmail.com>
2026-06-11 22:35:26 -0300

    feat: configurando deploy para o github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Diz ao Next para gerar HTML/CSS/JS puros na pasta 'out'
   images: {
-    unoptimized: true,
+    unoptimized: true, // Obrigatório se você estiver usando o componente <Image /> do Next
   },
-
-  basePath: '/atividade_cep_pweb2',
-  assetPrefix: '/atividade_cep_pweb2',
+  // Substitua 'nome-do-seu-repositorio' pelo nome exato do seu projeto no GitHub
+  basePath: process.env.NODE_ENV === 'production' ? '/atividade_cep_pweb2' : '',
 };
 
 export default nextConfig;
